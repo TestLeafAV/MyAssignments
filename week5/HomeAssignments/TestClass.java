1251ad684e6fbc1d1db9a581fa86e632ae03306f
TestLeafAV <aishwaryavijayanandan@gmail.com>
2024-08-23 22:45:03 +0530

    Update TestClass.java

--- week5/HomeAssignments/TestClass.java
+++ week5/HomeAssignments/TestClass.java
@@ -22,7 +22,7 @@ public class TestClass extends ProjectSpecificMethod{
 		driver.findElement(By.xpath("//mark[contains(text(),'Legal Entities')]")).click();
 		
 		//Click on New Legal Entity
-		driver.findElement(By.xpath("(//*[local-name()='svg' and @data-key='chevrondown'])[6]")).click();
+		driver.findElement(By.xpath("//one-app-nav-bar-item-root[@data-id='0QkJ4000000sbf9KAA']//*[local-name()='svg']")).click();
 		WebElement newL = driver.findElement(By.xpath("//a//span[text()='New Legal Entity']"));
 		driver.executeScript("arguments[0].click();", newL);
 		String len="Automation by Aish";
